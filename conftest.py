8f539108e5c0ad80d5b4b771afe5e4d916fcad41
Leefishking <754605301@qq.com>
2023-04-17 12:56:37 +0800

    我的第5次提交

--- conftest.py
+++ conftest.py
@@ -2,21 +2,22 @@ import pytest
 from pythoncode.calculator import Calculator
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="module")
 def myfixture():
     # print(f"\n执行我的fixture,里面的参数是:{request.param}\n")
     print("开始计算")
     yield  # 类似return
     print("结束计算")  # 清理数据，关闭数据库链接
 
-# def pytest_collection_modifyitems(session, config, items):
-#     print(type(items))
-#     items.reverse()
-#     for item in items:
-#         item.name = item.name.encode('utf-8').decode('unicode-escape')
-#         item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
-#
-#         if "add" in item._nodeid:
-#             item.add_marker(pytest.mark.add)
-#         if "div" in item._nodeid:
-#             item.add_marker(pytest.mark.div)
+
+def pytest_collection_modifyitems(session, config, items):
+    print(type(items))
+    items.reverse()  # 顺序翻转
+    for item in items:
+        item.name = item.name.encode('utf-8').decode('unicode-escape')
+        item._nodeid = item.nodeid.encode('utf-8').decode('unicode-escape')
+
+        if "add" in item._nodeid:
+            item.add_marker(pytest.mark.add)
+        if "div" in item._nodeid:
+            item.add_marker(pytest.mark.div)
